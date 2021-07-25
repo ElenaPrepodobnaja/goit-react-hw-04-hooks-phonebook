ded865bfcd9c7f0684859aa260d94756c47ffaef
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-07-25 16:06:54 +0300

    HW-4

--- src/App.js
+++ src/App.js
@@ -1,89 +1,145 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo } from 'react';
 import dataFromServer from './data/contacts.json';
 import Form from './components/Form';
 import Section from './components/Section';
 import Contacts from './components/Contacts';
 import Filter from './components/Filter';
 import { v4 as uuidv4 } from 'uuid';
+import useLocalStorage from './hooks/useLocalStorage';
 
-class App extends Component {
-  state = {
-    contacts: dataFromServer,
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', dataFromServer);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  formSubmitHandler = data => {
-    const { contacts } = this.state;
-    const isContactValid = this.validateContact(data, contacts);
-
-    if (isContactValid) {
-      data.id = uuidv4();
-      this.setState(({ contacts }) => ({
-        contacts: [data, ...contacts],
-      }));
-    }
-  };
-
-  validateContact = (data, contacts) => {
+  const validateContact = (data, contacts) => {
     if (contacts.some(({ name }) => name === data.name)) {
       alert(`${data.name} is already in contacts.`);
       return false;
     } else return true;
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const formSubmitHandler = data => {
+    const isContactValid = validateContact(data, contacts);
+
+    if (isContactValid) {
+      data.id = uuidv4();
+      setContacts(state => [data, ...state]);
+    }
   };
 
-  handleSearch = e => {
-    this.setState({
-      filter: e.currentTarget.value,
-    });
+  const deleteContact = id => {
+    setContacts(state => state.filter(contact => contact.id !== id));
   };
 
-  getFiltredContacts() {
-    const { contacts, filter } = this.state;
-    const lowerCaseFilter = filter.toLowerCase();
-    return contacts.filter(person =>
-      person.name.toLowerCase().includes(lowerCaseFilter),
-    );
-  }
+  const handleSearch = e => {
+    setFilter(e.currentTarget.value);
+  };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFiltredContacts();
+  const filteredContacts = useMemo(() => {
+    const getFiltredContacts = contacts => {
+      const lowerCaseFilter = filter.toLowerCase();
+      return contacts.filter(person =>
+        person.name.toLowerCase().includes(lowerCaseFilter),
+      );
+    };
+    return getFiltredContacts(contacts);
+  }, [contacts, filter]);
 
-    return (
-      <>
-        <Section title="Phonebook">
-          <Form onSubmit={this.formSubmitHandler} />
-        </Section>
+  return (
+    <>
+      <Section title="Phonebook">
+        <Form onSubmit={formSubmitHandler} />
+      </Section>
 
-        <Section title="Contacts">
-          <Filter value={filter} onChange={this.handleSearch} />
-          <Contacts
-            contacts={filteredContacts}
-            onDeleteBtnClick={this.deleteContact}
-          />
-        </Section>
-      </>
-    );
-  }
+      <Section title="Contacts">
+        <Filter value={filter} onChange={handleSearch} />
+        <Contacts
+          contacts={filteredContacts}
+          onDeleteBtnClick={deleteContact}
+        />
+      </Section>
+    </>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     contacts: dataFromServer,
+//     filter: '',
+//   };
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   formSubmitHandler = data => {
+//     const { contacts } = this.state;
+//     const isContactValid = this.validateContact(data, contacts);
+
+//     if (isContactValid) {
+//       data.id = uuidv4();
+//       this.setState(({ contacts }) => ({
+//         contacts: [data, ...contacts],
+//       }));
+//     }
+//   };
+
+//   validateContact = (data, contacts) => {
+//     if (contacts.some(({ name }) => name === data.name)) {
+//       alert(`${data.name} is already in contacts.`);
+//       return false;
+//     } else return true;
+//   };
+
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
+
+//   handleSearch = e => {
+//     this.setState({
+//       filter: e.currentTarget.value,
+//     });
+//   };
+
+//   getFiltredContacts() {
+//     const { contacts, filter } = this.state;
+//     const lowerCaseFilter = filter.toLowerCase();
+//     return contacts.filter(person =>
+//       person.name.toLowerCase().includes(lowerCaseFilter),
+//     );
+//   }
+
+//   render() {
+//     const { filter } = this.state;
+//     const filteredContacts = this.getFiltredContacts();
+
+//     return (
+//       <>
+//         <Section title="Phonebook">
+//           <Form onSubmit={this.formSubmitHandler} />
+//         </Section>
+
+//         <Section title="Contacts">
+//           <Filter value={filter} onChange={this.handleSearch} />
+//           <Contacts
+//             contacts={filteredContacts}
+//             onDeleteBtnClick={this.deleteContact}
+//           />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,68 +1,73 @@
-import { Component } from 'react';
+import { useState, useRef } from 'react';
 import s from './Form.module.css';
 import { v4 as uuidv4 } from 'uuid';
 
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function Form({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  nameId = uuidv4();
+  const nameId = useRef(uuidv4());
 
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleSubmit = e => {
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name: name, number: number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <div className={s.container}>
-        <form className={s.form} onSubmit={this.handleSubmit}>
-          <label htmlFor={this.nameId}>
-            <p className={s.form__label}>Name</p>
-            <input
-              type="text"
-              name="name"
-              className={s.form__firstInput}
-              value={name}
-              onChange={this.handleInputChange}
-              id={this.nameId}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              placeholder="Enter name"
-              required
-            />
-          </label>
-          <label>
-            <p className={s.form__label}>Number</p>
-            <input
-              type="tel"
-              name="number"
-              value={number}
-              onChange={this.handleInputChange}
-              maxLength="9"
-              minLength="7"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              required
-              placeholder="123-45-67"
-            ></input>
-          </label>
+  return (
+    <div className={s.container}>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <label htmlFor={nameId.current}>
+          <p className={s.form__label}>Name</p>
+          <input
+            type="text"
+            name="name"
+            className={s.form__firstInput}
+            value={name}
+            onChange={handleInputChange}
+            id={nameId.current}
+            placeholder="Enter name"
+            required
+          />
+        </label>
+        <label>
+          <p className={s.form__label}>Number</p>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleInputChange}
+            maxLength="9"
+            minLength="7"
+            pattern="[0-9]{3}-{0,1}[0-9]{2}-{0,1}[0-9]{2}"
+            required
+            placeholder="123-45-67"
+          />
+        </label>
+        <div className={s.submit__box}>
           <button
             className={s.form__button}
             type="submit"
@@ -70,10 +75,84 @@ class Form extends Component {
           >
             Add contact
           </button>
-        </form>
-      </div>
-    );
-  }
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Form;
+// class Form extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   nameId = uuidv4();
+
+//   handleInputChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+
+//     return (
+//       <div className={s.container}>
+//         <form className={s.form} onSubmit={this.handleSubmit}>
+//           <label htmlFor={this.nameId}>
+//             <p className={s.form__label}>Name</p>
+//             <input
+//               type="text"
+//               name="name"
+//               className={s.form__firstInput}
+//               value={name}
+//               onChange={this.handleInputChange}
+//               id={this.nameId}
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               placeholder="Enter name"
+//               required
+//             />
+//           </label>
+//           <label>
+//             <p className={s.form__label}>Number</p>
+//             <input
+//               type="tel"
+//               name="number"
+//               value={number}
+//               onChange={this.handleInputChange}
+//               maxLength="9"
+//               minLength="7"
+//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//               required
+//               placeholder="123-45-67"
+//             ></input>
+//           </label>
+//           <button
+//             className={s.form__button}
+//             type="submit"
+//             disabled={name === '' || number === ''}
+//           >
+//             Add contact
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Form;
