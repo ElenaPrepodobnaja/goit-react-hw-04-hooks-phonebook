5e2376ec4a6a0746de81ae847d34b5771509c957
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-07-25 15:06:38 +0300

    Начальная настройка

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from '../Contacts/Contacts.module.css';
+import PropTypes from 'prop-types';
+
+const Filter = ({ value, onChange }) => (
+  <label htmlFor="search">
+    <p className={s.search__label}>Find contacts by name</p>
+    <input
+      type="search"
+      name="search"
+      id="search"
+      onChange={onChange}
+      value={value}
+    />
+  </label>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
